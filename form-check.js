46806f3dd546147636771ddde612cda3224d0d87
Ayush <asharma619@rediffmail.com>
2024-03-09 16:31:48 +0530

    Added Functions

---
+++ form-check.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+
+function checkInputLength(){}
+
+function checkInputDataType(){}
+
+
+function checkFileType(){}
+
+
+function checkFileSize(){}
+
+
+function checkEmail(){}
+
+
+function castDataType(){}
+
